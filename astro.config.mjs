9c4c0c7eed667583e69fce188e5a9e691a8b3629
Kevin Tan <kevintan.me@gmail.com>
2026-06-04 19:23:39 +0700

    add site url for sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.indoenergytekniko.com",
   integrations: [react(), sitemap()],
 
   vite: {
